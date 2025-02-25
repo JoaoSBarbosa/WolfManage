b12c7ef8ffead58ff987f86449b098d70b3bdbcf
João <contato.jsbarbosa@gmail.com>
2025-02-24 21:43:11 -0300

    :tada: init- Criação da estrutura da fase 1 do projeto

---
+++ src/main/java/com/joaobarbosadev/WolfManage/WolfManageApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.WolfManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfManageApplication.class, args);
+	}
+
+}
